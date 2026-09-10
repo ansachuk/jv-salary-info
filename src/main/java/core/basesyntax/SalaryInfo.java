cdeed3b0cd18050366ad6e7e72452d7751d06792
Andrii Sachuk <ansachuk@icloud.com>
2026-09-10 12:13:00 +0200

    finished hw, created SalaryInfo method

--- src/main/java/core/basesyntax/SalaryInfo.java
+++ src/main/java/core/basesyntax/SalaryInfo.java
@@ -1,7 +1,39 @@
 package core.basesyntax;
 
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
 public class SalaryInfo {
+    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
     public String getSalaryInfo(String[] names, String[] data, String dateFrom, String dateTo) {
-        return null;
+        StringBuilder sb = new StringBuilder("Report for period ").append(
+                dateFrom).append(" - ").append(dateTo);
+
+        LocalDate from = LocalDate.parse(dateFrom, formatter);
+        LocalDate to = LocalDate.parse(dateTo, formatter);
+
+        for (String name : names) {
+            int totalSalary = 0;
+
+            for (String element : data) {
+                String[] fields = element.split(" ");
+
+                LocalDate current = LocalDate.parse(fields[0], formatter);
+                String currentName = fields[1];
+
+                if (!name.equals(currentName)) {
+                    continue;
+                }
+
+                if ((current.isAfter(from) || current.isEqual(from))
+                        && (current.isBefore(to) || current.isEqual(to))) {
+                    totalSalary += Integer.parseInt(fields[2]) * Integer.parseInt(fields[3]);
+                }
+            }
+
+            sb.append("\n").append(name).append(" - ").append(totalSalary);
+        }
+        return sb.toString();
     }
 }
